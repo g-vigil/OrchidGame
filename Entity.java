fc0f6551c1ea96d786c1ffcfea66709925a71929
g-vigil <140576005+g-vigil@users.noreply.github.com>
2023-12-25 20:00:29 -0600

    Create Entity.java

---
+++ Entity.java
@@ -0,0 +1,15 @@
+package entity;
+
+public class Entity 
+{
+
+    public int x, y;
+    public int speed;
+    
+}
+
+
+
+
+
+//This will be the Super class or Parent class and any other character classes
